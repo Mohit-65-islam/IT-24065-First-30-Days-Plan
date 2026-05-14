73aaad5c389c00dc017cd87cbcf208fb85296ee6
Manchineel <imohitul771@gmail.com>
2026-05-14 11:08:26 +0600

    Update Example1.java

--- Day-16 (04-05-2026)/Example1.java
+++ Day-16 (04-05-2026)/Example1.java
@@ -17,21 +17,3 @@ public class Example1 {
     }
 }
 ```
-# Example-2
-```java
-import java.io.FileInputStream;
-import java.io.IOException;
-
-public class Example2 {
-    public static void main(String[] args) throws IOException {
-        FileInputStream in = new FileInputStream("output.txt");
-
-        int data;
-        while ((data = in.read()) != -1) {
-            System.out.print((char) data);
-        }
-
-        in.close();
-    }
-}
-```
